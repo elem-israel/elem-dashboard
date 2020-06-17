8730a63bd18c394d212cb9bab7d9a449e9b7f4c5
karins28 <karins1010@gmail.com>
2020-06-18 00:13:29 +0300

    added cards back

--- src/views/RTLPage/Avatars.js
+++ src/views/RTLPage/Avatars.js
@@ -1,3 +1,12 @@
+import * as React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import a1 from "../../assets/img/avatars/1.jpg";
+import a2 from "../../assets/img/avatars/2.jpg";
+import a3 from "../../assets/img/avatars/3.jpg";
+import a4 from "../../assets/img/avatars/4.jpg";
+import a5 from "../../assets/img/avatars/5.jpg";
+import a6 from "../../assets/img/avatars/6.jpg";
+import CardBody from "../../components/Card/CardBody";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import IconButton from "@material-ui/core/IconButton";
 import Button from "@material-ui/core/Button";
