bc24a5948e71ebac080d9d3b21109d7d960e351a
imri <imri.paran@gmail.com>
2020-06-17 08:07:19 +0300

    fixed avatar

--- src/views/RTLPage/Avatars.js
+++ src/views/RTLPage/Avatars.js
@@ -5,7 +5,7 @@ import a2 from "../../assets/img/avatars/2.jpg";
 import a3 from "../../assets/img/avatars/3.jpg";
 import a4 from "../../assets/img/avatars/4.jpg";
 import a5 from "../../assets/img/avatars/5.jpg";
-import a6 from "../../assets/img/avatars/5.jpg";
+import a6 from "../../assets/img/avatars/6.jpg";
 import CardBody from "../../components/Card/CardBody";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import IconButton from "@material-ui/core/IconButton";
