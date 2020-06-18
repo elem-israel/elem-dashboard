383cb8034e01d35f87ec76abb3695649c0a74f92
imri <imri.paran@gmail.com>
2020-06-18 08:29:01 +0300

    optimized imports

--- src/views/RTLPage/Graphs.js
+++ src/views/RTLPage/Graphs.js
@@ -1,17 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import CardFooter from "../../components/Card/CardFooter";
-import SearchIcon from "@material-ui/icons/Search";
-import ReportProblemOutlinedIcon from "@material-ui/icons/ReportProblemOutlined";
-import ErrorOutlineIcon from "@material-ui/icons/ErrorOutline";
-import Update from "@material-ui/icons/Update";
 import GridItem from "../../components/Grid/GridItem";
 import Card from "../../components/Card/Card";
 import CardHeader from "../../components/Card/CardHeader";
-import CardIcon from "../../components/Card/CardIcon";
-import LocalOffer from "@material-ui/icons/LocalOffer";
 import GridContainer from "../../components/Grid/GridContainer";
-import ChartistGraph from "react-chartist";
-import { completedTasksChart, dailySalesChart } from "../../variables/charts";
 import CardBody from "../../components/Card/CardBody";
 import ArrowUpward from "@material-ui/icons/ArrowUpward";
 import AccessTime from "@material-ui/icons/AccessTime";
