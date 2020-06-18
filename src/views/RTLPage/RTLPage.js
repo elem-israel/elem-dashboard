07c7718891c6f67332037a4f454ef1743c5f1892
imri <imri.paran@gmail.com>
2020-06-18 06:37:01 +0300

    optimized imports

--- src/views/RTLPage/RTLPage.js
+++ src/views/RTLPage/RTLPage.js
@@ -1,9 +1,9 @@
 /*eslint-disable*/
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 // react plugin for creating charts
 import ChartistGraph from "react-chartist";
 // @material-ui/core
-import { makeStyles } from "@material-ui/core/styles";
+import {makeStyles} from "@material-ui/core/styles";
 import Icon from "@material-ui/core/Icon";
 // @material-ui/icons
 import LocalOffer from "@material-ui/icons/LocalOffer";
@@ -11,28 +11,17 @@ import Update from "@material-ui/icons/Update";
 import ArrowUpward from "@material-ui/icons/ArrowUpward";
 import AccessTime from "@material-ui/icons/AccessTime";
 import Accessibility from "@material-ui/icons/Accessibility";
-import BugReport from "@material-ui/icons/BugReport";
-import Code from "@material-ui/icons/Code";
-import Cloud from "@material-ui/icons/Cloud";
 // core components
 import Button from "components/CustomButtons/Button.js";
 import GridItem from "components/Grid/GridItem.js";
 import GridContainer from "components/Grid/GridContainer.js";
-import Table from "components/Table/Table.js";
-import Tasks from "components/Tasks/Tasks.js";
-import CustomTabs from "components/CustomTabs/CustomTabs.js";
 import Card from "components/Card/Card.js";
 import CardHeader from "components/Card/CardHeader.js";
 import CardIcon from "components/Card/CardIcon.js";
 import CardBody from "components/Card/CardBody.js";
 import CardFooter from "components/Card/CardFooter.js";
 import SnackbarContent from "components/Snackbar/SnackbarContent.js";
-import Avatar from "@material-ui/core/Avatar";
-
-import a3 from "../../assets/img/avatars/3.jpg";
-
-import { completedTasksChart, dailySalesChart } from "variables/charts.js";
-
+import {completedTasksChart, dailySalesChart} from "variables/charts.js";
 import styles from "assets/jss/material-dashboard-react/views/rtlStyle.js";
 import Avatars from "./Avatars";
 import Highlight from "./Highlight";
